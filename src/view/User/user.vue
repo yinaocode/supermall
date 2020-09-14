4c35337ff67409534d72718cc26ee028e0390bd9
yinaocode <2976039779@qq.com>
2020-09-14 21:39:20 +0800

    添加用户弹出框和输入信息验证

--- src/view/User/user.vue
+++ src/view/User/user.vue
@@ -11,12 +11,17 @@
       <!-- 搜索与添加区 -->
       <el-row :gutter="20">
         <el-col :span="7">
-          <el-input placeholder="请输入内容" class="input-with-select">
-            <el-button slot="append" icon="el-icon-search"></el-button>
+          <el-input placeholder="请输入内容" 
+          v-model="queryInfo.query" 
+           clearable
+           @clear="getUserList()">
+            <el-button slot="append" icon="el-icon-search"
+            @click="getUserList()"></el-button>
           </el-input>
         </el-col>
         <el-col :span="4">
-          <el-button type="primary" icon="el-icon-search">添加用户</el-button>
+          <el-button type="primary" icon="el-icon-search"
+          @click="addDialogVisible=true">添加用户</el-button>
         </el-col>
       </el-row>
       <!-- 列表区 -->
@@ -48,19 +53,63 @@
       <el-pagination
         @size-change="handleSizeChange"
         @current-change="handleCurrentChange"
-        :current-page="pagenum"
+        :current-page="queryInfo.pagenum"
         :page-sizes="[5, 10, 20, 50]"
-        :page-size="pagesize"
+        :page-size="queryInfo.pagesize"
         layout="total, sizes, prev, pager, next, jumper"
         :total="total"
       ></el-pagination>
     </el-card>
+    <!-- 添加用户对话框 -->
+    <el-dialog
+  title="添加用户"
+  :visible.sync="addDialogVisible"
+  @close="addDialogClose()"
+  width="50%"
+  >
+  <!-- 内容主体区 -->
+  <el-form :model="addForm" :rules="addFormRules" ref="addFormRulesRef" label-width="70px" >
+  <el-form-item label="用户名" prop="username">
+    <el-input v-model="addForm.username"></el-input>
+  </el-form-item>
+   <el-form-item label="密码" prop="password">
+    <el-input  type="password" v-model="addForm.password"></el-input>
+  </el-form-item>
+   <el-form-item label="邮箱" prop="email">
+    <el-input v-model="addForm.email"></el-input>
+  </el-form-item>
+   <el-form-item label="手机" prop="mobile">
+    <el-input v-model="addForm.mobile"></el-input>
+  </el-form-item>
+  </el-form>
+   <!-- 内容底部 -->
+  <span slot="footer" class="dialog-footer">
+    <el-button @click="addDialogVisible = false">取 消</el-button>
+    <el-button type="primary" @click="addDialogVisible = false">确 定</el-button>
+  </span>
+</el-dialog>
   </div>
 </template>
 
 <script>
 export default {
   data() {
+     // 验证邮箱的规则
+      var checkEmail = (rule,value,cb)=>{
+        const regEmail = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(\.[a-zA-Z0-9_-])+/
+        if(regEmail.test(value)){
+          return cb()
+        }
+        return cb(new Error('请输入合法的邮箱'))
+     };
+         // 验证手机号的规则
+       var checkMobile = (rule,value,cb)=>{
+ const regMobile = /^(1)[0-9]{9}[0-9]$/
+        if(regMobile.test(value)){
+          return cb()
+        }
+        return cb(new Error('请输入合法的手机号'))
+      };
     return {
       // 获取用户列表的参数对象
       queryInfo: {
@@ -70,6 +119,25 @@ export default {
       },
       userList: [],
       total: 0,
+      addDialogVisible:false,//添加用户对话框的显示与隐藏
+   addForm:{
+     username:'',
+     password:'',
+     email:'',
+     mobile:''
+   },//添加用户表单数据
+   addFormRules:{
+     username:[{required:true,message:'请输入用户名',trigger:'blur'},
+       {min:3,max:10,message:'用户名长度在3~10之间',trigger:'blur'}],
+      password:[{required:true,message:'请输入密码',trigger:'blur'},
+       {min:6,max:15,message:'用户名长度在6~15之间',trigger:'blur'}],
+     email:[{required:true,message:'请输入邮箱',trigger:'blur'},
+       {validator:checkEmail,trigger:'blur'}],
+      mobile:[{required:true,message:'请输入手机号',trigger:'blur'},
+         {validator:checkMobile,trigger:'blur'}],
+
+     
+   },//添加用户表单验证
     };
   },
   created() {
@@ -103,6 +171,10 @@ export default {
        return this.$message.error('更改用户状态失败')
       }
      return this.$message.success('更改用户状态成功')
+    },
+    // 监听用户表单关闭事件
+    addDialogClose(){
+      this.$refs.addFormRulesRef.resetFields()
     }
   },
 };
