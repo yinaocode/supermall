e8ec0b982d1c35a62ab30b263e11ace78bb65561
yinaocode <2976039779@qq.com>
2020-10-09 22:27:54 +0800

    优化

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,28 @@
-module.export = {
-
+module.exports = {
+    chainWebpack: config => {
+        // 发布模式
+        config.when(process.NODE_ENV === 'production', config => {
+            config.entry('app').clear().add('./src/main.js')
+            // config.set('externals',{
+            //     vue:'vue',
+            //     'vue-router':'vue-router',
+            //     axios:'axios',
+            //     echarts:'echarts',
+            //     nprogerss:'Nprogress',
+            //     'vue-quill-editor':'VueQuillEditor'
+            // })
+            config.plugin('html').tap(args => {
+                args[0].isProd = true
+                return args
+            })
+        })
+        // 开发模式
+        config.when(process.NODE_ENV === 'development', config => {
+            config.entry('app').clear().add('./src/main-dev.js')
+            config.plugin('html').tap(args => {
+                args[0].isProd = false
+                return args
+            })
+        })
+    }
 }
