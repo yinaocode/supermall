a3a37b114b500c06cbd010417da26cd693253733
yinaocode <2976039779@qq.com>
2020-10-10 21:40:30 +0800

    修改element-ui引用

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router/index.js'
-// import ElementUI from 'element-ui'
-// import 'element-ui/lib/theme-chalk/index.css';
-// Vue.use(ElementUI)
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI)
 // 导入富文本编辑器
 import VueQuillEditor from 'vue-quill-editor'
 import 'quill/dist/quill.core.css' // import styles
